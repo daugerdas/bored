a4a8868b2166a0902b06eff7bb534388ce982598
Karolis Daugerdas <daugerdas@gmail.com>
2023-08-17 22:39:18 +0300

    add basic functionality

--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,56 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import logo from "./logo.svg";
+import {
+  useQuery,
+  QueryClient,
+  QueryClientProvider,
+  useQueryClient,
+} from "@tanstack/react-query";
+import { ChakraProvider } from "@chakra-ui/react";
+import { Card, CardHeader, CardBody, Button, Spinner } from "@chakra-ui/react";
+import "./App.css";
+
+const queryClient = new QueryClient();
+
+async function getInfo() {
+  const response = await fetch("http://www.boredapi.com/api/activity/");
+  return response.json();
+}
+
+function BoredCard() {
+  const queryClient = useQueryClient();
+  const { data, isLoading } = useQuery({
+    queryKey: ["info"],
+    queryFn: getInfo,
+  });
+  console.log(data?.activity);
+  if (isLoading)
+    return (
+      <div>
+        <Spinner />
+      </div>
+    );
+  else
+    return (
+      <div className="container">
+        <Card>
+          <CardHeader>Bored?</CardHeader>
+          <CardBody>{data.activity}</CardBody>
+          <Button colorScheme="blue" onClick={() => window.location.reload()}>
+            Refresh
+          </Button>
+        </Card>
+      </div>
+    );
+}
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <QueryClientProvider client={queryClient}>
+      <ChakraProvider>
+        <BoredCard />
+      </ChakraProvider>
+    </QueryClientProvider>
   );
 }
 
